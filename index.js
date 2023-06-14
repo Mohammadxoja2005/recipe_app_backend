8379cdffcdca61921421c014a05a8d83025d202a
Mohammadxoja2005 <rustamxojayev.muhammadxoja@yandex.ru>
2023-06-14 23:59:31 +0500

    added mysql2 to production mode

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use("/comment", commentRouter);
 db.sequelize
     .sync()
     .then(() => {
-        app.listen(process.env.PORT || 3002, () => {
+        app.listen(process.env.PORT || 3004, () => {
             console.log("server started...");
         });
     })
