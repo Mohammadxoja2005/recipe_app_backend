5eae7533ec17e1fd3c8f9fd48e233e24beb6a2ce
Mohammadxoja2005 <rustamxojayev.muhammadxoja@yandex.ru>
2023-06-15 03:51:34 +0500

    managed database connection and added pool

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use("/comment", commentRouter);
 db.sequelize
     .sync()
     .then(() => {
-        app.listen(process.env.PORT || 3000, () => {
+        app.listen(process.env.PORT || 3008, () => {
             console.log("server started...");
         });
     })
