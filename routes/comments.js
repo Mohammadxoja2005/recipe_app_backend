17e3e40c6728f475bc6100376732793ff588f050
Mohammadxoja2005 <rustamxojayev.muhammadxoja@yandex.ru>
2023-06-16 04:21:31 +0500

    added API for getting comments

--- routes/comments.js
+++ routes/comments.js
@@ -2,6 +2,14 @@ const express = require("express");
 const router = express.Router();
 const { comment } = require('../models')
 
+router.get('/:id', async (req, res) => {
+    const { id } = req.params;
+
+    const response = await comment.findAll({ where: { recipe_id: id } })
+
+    res.json(response);
+})
+
 router.post('/create/:id', async (req, res) => {
     const { id } = req.params;
     const { commentToRecipe } = req.body;
